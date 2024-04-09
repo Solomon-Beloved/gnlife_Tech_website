03e349cdadade9ef49107c0cd56080be843456c0
Temitope-Beloved <olafusisam236@gmail.com>
2024-04-08 18:50:08 -0700

    finish designing the navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
+import {Routes , Router} from 'react-router-dom'
 import './App.css'
+import Homepage from './Pages/Homepage/Homepage'
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Homepage/>
     </div>
   )
 }
